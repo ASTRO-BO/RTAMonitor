209740caaeac8483aa2c2d9007cd0df765637ea3
Andrea Zoli <zoli@iasfbo.inaf.it>
2014-03-25 15:43:34 +0100

    RTAMonitor.py Visualize EBSim rate.

--- RTAMonitor.py
+++ RTAMonitor.py
@@ -2,8 +2,8 @@
 
 ############################################################################
 #    begin                : Mar 25 2014
-#    copyright            : (C) 2014 Andrea Zoli
-#    email                : zoli@iasfbo.inaf.it
+#    copyright            : (C) 2014 Andrea Zoli, Valentina Fioretti
+#    email                : zoli@iasfbo.inaf.it, fioretti@iasfbo.inaf.it
 ############################################################################
 
 ############################################################################
@@ -22,17 +22,42 @@ Ice.loadSlice('RTACommand.ice')
 Ice.updateModules()
 import CTA
 
+from traits.api \
+    import HasTraits, Range, Float, String, on_trait_change, Property
+from traitsui.api import View, Item
+
 command = 0
 
+class Gui(HasTraits):
+
+    simDelay = Range(0.0, 2000.0)
+    simRate = String("0 MB/s")
+
+    traits_view = View(Item(name='simDelay', style='simple'),
+                       Item(name='simRate', style='readonly'),
+                       resizable=True,
+                       title='RTA Sleep',
+                       width=500, height=200)
+
+    def __init__(self):
+        HasTraits.__init__(self)
+
+    def _simDelay_changed(self):
+        print("c")
+
+
 class MonitorI(CTA.RTAMonitor):
-    def __init__(self, ic):
+    def __init__(self, ic, gui):
         CTA.RTAMonitor.__init__(self)
         self.ic = ic
+        self.gui = gui
 
     def sendParameter(self, param, current=None):
         print("Received a Parameter")
         if param.type == 0:
-            print "apid:", param.apid, "time:", param.timestamp, "rate:", param.value, "MB/s"
+#            print "apid:", param.apid, "time:", param.timestamp, "rate:", param.value, "MB/s"
+            if param.apid == 200:
+                self.gui.simRate = str(param.value)+" MB/s"
 
     def sendLog(self, msg, current=None):
         print(msg.apid, msg.timestamp, msg.value)
@@ -49,13 +74,15 @@ class Monitor(Ice.Application):
             print(self.appName() + ": too many arguments")
             return 1
 
+        gui = Gui()
+
 		# Activate RTAMonitor adapter
         ic = self.communicator()
         adapter = ic.createObjectAdapter("RTAMonitor")
-        adapter.add(MonitorI(ic), ic.stringToIdentity("monitor"))
+        adapter.add(MonitorI(ic, gui), ic.stringToIdentity("monitor"))
         adapter.activate()
 
-        ic.waitForShutdown()
+        gui.configure_traits()
         return 0
 
 if __name__ == "__main__":
